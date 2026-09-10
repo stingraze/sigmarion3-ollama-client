0836f9f93fd87e134a4f79d8fc2c6d2d0becd387
Tsubasa Kato <tsubasa@superai.online>
2026-09-10 19:51:04 +0900

    adding more files

---
+++ .toolchain/cegcc/lib/gcc/arm-mingw32ce/9.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/runner/work/cegcc-build/cegcc-build/gcc/configure --prefix=/opt/cegcc --build=x86_64-pc-linux-gnu --with-gcc --with-gnu-ld --with-gnu-as --enable-threads=win32 --disable-nls --enable-languages=c,c++ --disable-win32-registry --disable-multilib --disable-interwork --without-newlib --enable-checking --with-headers --disable-__cxa_atexit --build=x86_64-linux-gnu --host=x86_64-pc-linux-gnu --target=arm-mingw32ce";
+static const char thread_model[] = "win32";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm7tdmi" } };
